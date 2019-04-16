36609e8e64fb3c1b4266a79d75f7730d455f6036
yongpeng1 <yongpeng1@staff.sina.com.cn>
2019-04-16 19:51:54 +0800

    添加：register

---
+++ common/register/register.go
@@ -0,0 +1,14 @@
+package register
+
+import (
+	"luyaops/fw/common/etcd"
+	"luyaops/fw/common/log"
+)
+
+func Register(store *etcd.Store, server, apiJson string) {
+	if err := store.Put(server, apiJson); err != nil {
+		log.Errorf("register %v server error", server)
+	} else {
+		log.Infof("register %v server successfully", server)
+	}
+}
