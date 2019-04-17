6d6a6016843d74fe6b34c04ebcae351d123d693b
yongpeng1 <yongpeng1@staff.sina.com.cn>
2019-04-17 13:56:03 +0800

    修改：register error 日志输出

--- common/register/register.go
+++ common/register/register.go
@@ -7,7 +7,7 @@ import (
 
 func Register(store *etcd.Store, server, apiJson string) {
 	if err := store.Put(server, apiJson); err != nil {
-		log.Errorf("register %v server error", server)
+		log.Errorf("register %v server error:%v", server, err)
 	} else {
 		log.Infof("register %v server successfully", server)
 	}
