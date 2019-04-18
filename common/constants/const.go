c68e1d790b50147990061c1de225bbce7820838f
yongpeng1 <yongpeng1@staff.sina.com.cn>
2019-04-18 15:02:00 +0800

    增加：注册信息前缀

--- common/constants/const.go
+++ common/constants/const.go
@@ -1,6 +1,7 @@
 package constants
 
 const (
+	RegistryPrefix   = "rpc_server/"
 	RpcServerPort    = "50051"
 	HttpRequest      = "http_request"
 	RpcMethodOptions = "rpc_method_options"
--- common/register/register.go
+++ common/register/register.go
@@ -1,12 +1,13 @@
 package register
 
 import (
+	"github.com/luyaops/fw/common/constants"
 	"github.com/luyaops/fw/common/etcd"
 	"github.com/luyaops/fw/common/log"
 )
 
 func Register(store *etcd.Store, server, apiJson string) {
-	if err := store.Put(server, apiJson); err != nil {
+	if err := store.Put(constants.RegistryPrefix+server, apiJson); err != nil {
 		log.Fatalf("Register %v server error:%v", server, err)
 	} else {
 		log.Infof("Register %v server successfully", server)
